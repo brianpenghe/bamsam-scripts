436ff71fc9488f770eb307828bba70d342d63e9d
brianpenghe <brianpenghe@gmail.com>
2016-05-02 15:33:32 -0700

    Henry fixed a bug related to pysam version update

--- resample_bam.py
+++ resample_bam.py
@@ -8,7 +8,7 @@ import numpy as np
 
 def calculate_samples(bamfile, count, ref_prefix=None):
     refcounts = dict()
-    for s in pysam.idxstats(bamfile):
+    for s in pysam.idxstats(bamfile).split("\n"):
         tok = s.rstrip().split("\t")
         if ref_prefix is not None and tok[0].startswith(ref_prefix) == False:
             continue
@@ -27,6 +27,8 @@ def get_random_alignments(bfd, obfd, ref, refcounts):
     q = sorted(np.random.permutation(np.arange(total))[:count])
     qi = 0
     algni = 0
+    if len(q) == 0:
+        return
     for algn in bfd.fetch(ref):
         if algni == q[qi]:
             obfd.write(algn)
@@ -59,7 +61,7 @@ if __name__ == "__main__":
     sortedprefix = "%s.sorted" % ".".join(args.outfile.split(".")[:-1])
     if args.verbose:
         print "sorting %s -> %s.bam" % (args.outfile, sortedprefix)
-    pysam.sort(args.outfile, sortedprefix)
+    pysam.sort("-o", "%s.bam" % sortedprefix, args.outfile)
     shutil.move("%s.bam" % sortedprefix, args.outfile)
     if args.verbose:
         print "indexing %s" % args.outfile
